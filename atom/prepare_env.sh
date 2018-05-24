de91f4215f5be56d5e2dc6f5ae91c727831fc09c
unknown <song.w@song7w10.home.langchao.com>
2018-05-24 16:42:16 +0800

    update prepare_env

--- atom/prepare_env.sh
+++ atom/prepare_env.sh
@@ -8,17 +8,18 @@ if [ $? -eq 0 ];then
 	echo "The qemu-utils tools already installed"
 else
     apt-get install qemu-utils -y
-
+fi
 #Determine virtinst tools install
 dpkg -l|grep -E "virtinst"|awk '{print$2}'|grep -E "^virtinst"
 if [ $? -eq 0 ];then
 	echo "The virtinst tools already installed"
 else
     apt-get install virtinst -y
-
+fi
 #Determine libguestfs-tools tools install
 dpkg -l|grep -E "libguestfs-tools"|awk '{print$2}'|grep -E "^libguestfs-tools"
 if [ $? -eq 0 ];then
 	echo "The libguestfs-tools  already installed"
 else
-    apt-get install libguestfs-tools -y   
+    apt-get install libguestfs-tools -y
+fi
